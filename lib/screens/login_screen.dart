27817defbe3285b29725c1d1e7bdff76221cf590
adrigm <adrigm.admin@gmail.com>
2022-02-23 20:15:11 +0100

    Fist commit

---
+++ lib/screens/login_screen.dart
@@ -0,0 +1,28 @@
+import 'package:flutter/material.dart';
+import 'package:productos_app/widgets/widgets.dart';
+
+class LoginScreen extends StatelessWidget {
+  const LoginScreen({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: AuthBackground(
+          child: SingleChildScrollView(
+        child: Column(
+          children: [
+            const SizedBox(height: 250),
+            CardContainer(
+                child: Column(
+              children: [
+                const SizedBox(height: 10),
+                Text('Login', style: Theme.of(context).textTheme.headline4),
+                const SizedBox(height: 30),
+              ],
+            )),
+          ],
+        ),
+      )),
+    );
+  }
+}
